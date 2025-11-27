4b944c9bbec5b9b11b5f964388491b20234f436d
chetan Paudel <paudelchetan1112@gmail.com>
2025-11-27 08:27:28 +0545

    all value print of list using For loop

---
+++ ForLoopusingSequentialDataTypes.py
@@ -0,0 +1,8 @@
+#For loops usiing Sequential Data types
+fruits_name=["apple","banana","orange"]
+for fruit in fruits_name:
+    print(fruit)
+#print list of Number 
+number=[10,20,30,40,50]
+for n in number:
+    print(n)
