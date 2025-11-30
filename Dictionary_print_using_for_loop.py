4289b9c66c4e7cf0a2ca3dcd0822da16e9913f25
chetan Paudel <paudelchetan1112@gmail.com>
2025-11-30 08:00:28 +0545

    dictionary and list print using for loop

---
+++ Dictionary_print_using_for_loop.py
@@ -0,0 +1,3 @@
+student_details={"name":"ram", "age":21, "faculty":"BCA"}
+for key in student_details:
+   print(key,":",student_details[key])
---
+++ listwithforloop.py
@@ -0,0 +1,10 @@
+fruits=["mango","banana","orange"]
+for n in fruits:
+    print(n)
+
+
+list=[1,2,3,4,5]
+for n in list:
+    print(n)
+
+
