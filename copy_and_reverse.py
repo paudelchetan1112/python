18ba6e6dfe30db3d7ec7681cdbb5778e565ddf25
chetan Paudel <paudelchetan1112@gmail.com>
2025-12-02 08:25:46 +0545

    copy, sort, reverse, clear

---
+++ copy_and_reverse.py
@@ -0,0 +1,18 @@
+animals=["cow","buffalo","cow"]
+animalsCopy=animals.copy()
+print(animalsCopy)
+print(animals)
+
+
+# print(animals.reverse())
+
+print(animals.count("cow"))
+
+animals.sort()
+# for animal in animals:
+#     print(animal)
+
+print(animals)
+
+animals.clear()
+print(animals)
