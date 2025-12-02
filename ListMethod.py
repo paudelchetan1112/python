ba2e95e1d3cd89736411f6ccb61b977b8c1a1cc3
chetan Paudel <paudelchetan1112@gmail.com>
2025-12-02 08:08:50 +0545

    extends and remove method.py

---
+++ ListMethod.py
@@ -0,0 +1,17 @@
+
+#extend Method
+animals=["cat","dog"]
+birds=["parrot","parrot", "eagle"]
+animals.extend(birds)
+print(animals)
+print(birds)
+
+# remove Method
+
+animals.remove('parrot')
+
+
+print(animals)
+
+
+
