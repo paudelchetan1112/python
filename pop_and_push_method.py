be0354791081cd6f7443537d6c82d54d293141df
chetan Paudel <paudelchetan1112@gmail.com>
2025-12-02 08:17:24 +0545

    pop and insert and append

---
+++ pop_and_push_method.py
@@ -0,0 +1,13 @@
+animals=["cat", "dog", "rabbit"]
+animals.pop(1)
+print(animals)
+print(animals.index("rabbit"))
+
+student_name=["sudip", "chetan", "silwal", "bittu"]
+student_name.append("sugam")
+print(student_name)
+
+student_name.insert(2,"anup")
+
+print(student_name)
+
