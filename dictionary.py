947c93c623de3d051af7053c17367ed18fa540f3
chetan Paudel <paudelchetan1112@gmail.com>
2025-12-03 08:16:00 +0545

    some dictionary method

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,25 @@ for key, value in user.items():
 
 print(user.keys()) # print all keys
 print(user.values()) #print all value of key
+
+
+student_data={"username":"student1", "age":21}
+#adding new key value pair
+student_data["address"]="npj"
+
+
+#update key value 
+print(student_data)
+user.update({'online':False, 'followers':1000})
+print(user)
+
+# delete key value
+del user['status']
+print(user)
+
+#to clear dictionary
+user.clear()
+print(user)
+
+
+
